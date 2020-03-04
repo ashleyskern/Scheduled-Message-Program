3224ba092733e860de1ebc6cbf70ed644fd9ad11
ashleyskern <60665967+ashleyskern@users.noreply.github.com>
2020-03-03 20:26:51 -0800

    File upload

---
+++ ScheduledMessageProgramming.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <style type="text/css">
+
+    .bold {
+    font-weight:bold;
+    }
+    body {
+      background-color: lightblue;
+      color: white;
+}
+
+h1 {
+  text-decoration: overline;
+  text-shadow: 15px 14px grey;
+  font-size: 150pt;
+
+}
+
+
+    </style>
+
+
+  </head>
+  <body>
+    <?php
+
+    date_default_timezone_set('America/Los_Angeles');
+
+      //This is a PHP script that knows which day of the week it is
+
+      //First we get the week day as an integer using a build-in PHP function
+      $dayofweek = date("w");
+
+      //Then we use a switch to give a message depending on the day it is
+      switch ($dayofweek) {
+        case 1:
+          echo "<h1><center>It is Monday!</center></h1>";
+          break;
+        case 2:
+          echo "<h1><center>It is Tuesday!</center></h1>";
+          break;
+        case 3:
+          echo "<h1><center>It is Wednesday!</center></h1>";
+          break;
+        case 4:
+          echo "<h1><center>It is Thursday!</center></h1>";
+          break;
+        case 5:
+          echo "<h1><center>It is Friday!</center></h1>";
+          break;
+        case 6:
+          echo "<h1><center>It is Saturday!</center></h1>";
+          break;
+        case 0:
+          echo "<h1><center>It is Sunday!</center></h1>";
+          break;
+        default:
+          echo "<h1><center>Error!</center></h1>";
+          break;
+
+      }
+
+      //Our YYYY-MM-DD date string.
+    echo "</br>";
+    echo "</br>";
+
+
+    echo "<h3><center>The time is " . date("h:i:sa</center></h3>");
+
+
+    ?>
+  </body>
+</html>
